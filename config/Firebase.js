03ab5b7bc9f3e4624579ff3aeabf212924cdb0d4
Rustam-Deniz Emirali <rustam_de@powr.io>
2020-04-20 19:05:01 +0600

    small fix

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHgz2lJOhN-AuatTcR3SHIsdorNct8rlY",
-    authDomain: "react-native-auth-54d86.firebaseapp.com",
-    databaseURL: "https://react-native-auth-54d86.firebaseio.com",
-    projectId: "react-native-auth-54d86",
-    storageBucket: "react-native-auth-54d86.appspot.com",
-    messagingSenderId: "863151849676",
-    appId: "1:863151849676:web:5a83f62e19c35b252c8269",
-    measurementId: "G-5HKWKF7SJV"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 const Firebase = firebase.initializeApp(firebaseConfig)
